5c6ac629f11d8f12daf7394766413a2011d6e755
Cody G <girardc101@outlook.com>
2026-06-18 21:44:54 -0400

    Fixed environment variable name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Database, Tables } from '../database.types';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_PUBLISHABLE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 let supabase: SupabaseClient<Database>;
 if (supabaseUrl != null && supabaseKey != null) {
   supabase = createClient<Database>(supabaseUrl, supabaseKey, {

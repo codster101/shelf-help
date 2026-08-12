07241075d5aaebc3b965d4b6d77f305a34fa8f77
Cody G <girardc101@outlook.com>
2026-08-11 19:28:21 -0500

    started product sync

---
+++ app/product_sync.ts
@@ -0,0 +1,10 @@
+export { }
+
+const response = await fetch("https://www.marykay.com/en/updategrid?cgid=root&srule=product-name-ascending&start=0&sz=10000");
+
+if (!response.ok) {
+	throw new Error(`Response status: ${response.status}`);
+}
+
+const result = await response.text();
+console.log(result);
